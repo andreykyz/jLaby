39ea69834f0d5ccbc0f09858c25e69ec0ef51d69
Kyznecov Andrey <andreykyz@gmail.com>
2012-12-23 21:07:45 +0400

    add Ant interface

---
+++ src/devhead/ru/jlaby/Ant.java
@@ -0,0 +1,19 @@
+package devhead.ru.jlaby;
+/**
+ * 
+ * @author Kuznetsov Andrey
+ *
+ */
+public interface Ant {
+
+	public void right();
+
+	public void left();
+
+	public void forward();
+
+	public void take();
+
+	public void drop();
+
+}
